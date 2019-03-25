5b19f6d234857a71c2a3f8bedc2c43bbf1472076
Ed <carpio.ed@gmail.com>
2019-03-25 05:28:33 -0700

    first commit

---
+++ scripts/main.min.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    var options = {
+        mobileFallbackImage: "http://www.hdwallpapers.in/walls/pink_cosmos_flowers-wide.jpg",
+        playOnlyIfVisible  : false
+    };
+
+    myPlayer = $(".player").YTPlayer(options);
+
+})
